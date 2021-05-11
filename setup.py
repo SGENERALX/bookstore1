594bf699312fe96d57d39dc547375ba3c5563023
Youta Obby Sidane <dyosby237@gmail.com>
2021-05-11 21:16:49 +0100

    :bug: Fix all

--- setup.py
+++ setup.py
@@ -2,15 +2,7 @@
 Created on 23-Feb-2016
 @author: parkar_s
 '''
-import os
 from setuptools import setup
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='Online Book Store',
@@ -23,7 +15,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     platforms='any',
-    packages=['BookStore'],
+    packages=['bookstore'],
     install_requires=[
         'virtualenv>=14.0.3',
         'Flask>=0.10.1',
