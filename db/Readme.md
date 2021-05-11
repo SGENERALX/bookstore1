736bb6df8f3d84680d86bef06db5a7773bff9967
Youta Obby Sidane <dyosby237@gmail.com>
2021-05-11 19:35:58 +0100

    :bug: Fix sql file

---
+++ db/Readme.md
@@ -0,0 +1,11 @@
+Migrations Orders:
+
+- create_table.sql
+- category.sql
+- sub_cat.sql
+- customer.sql
+- book.sql
+- feedback.sql
+- rating.sql
+- wishlist.sql
+- address.sql
--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
-'''
-Created on 23-Feb-2016
-
-@author: parkar_s
-'''
+import os
 from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
-    name='Online Book Store',
+    name='WeLearn',
     version=1.0,
-    url='https://github.com/siddhiparkar151992/Py_Shopping_Cart',
-    author='Siddhi Parkar',
-    author_email='siddhiparkar15@gmail.com',
+    author = "Deprez",
+    author_email = "deprez@gmail.com",
+    url='http://localhost',
     description='A shopping cart',
-    long_description=__doc__,
     include_package_data=True,
     zip_safe=False,
     platforms='any',
-    packages=['BookStore'],
     install_requires=[
         'virtualenv>=14.0.3',
         'Flask>=0.10.1',
@@ -24,5 +26,4 @@ setup(
         'pymongo>=3.2.3',
         'jsonpickle>= 0.9.3'
     ]
-      
 )
